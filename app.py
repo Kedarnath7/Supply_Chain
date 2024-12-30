b75ac571df5638101446e4799493c7375aef766b
Kedarnath7 <139032493+Kedarnath7@users.noreply.github.com>
2024-12-30 18:50:36 +0530

    Model training and Pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.supply_chain_optimization.exception import CustomException
 import sys
 from src.supply_chain_optimization.components.data_ingestion import DataIngestion
 from src.supply_chain_optimization.components.data_ingestion import DataIngestionConfig
-from src.supply_chain_optimization.pipelines.transform_pipeline import transformer_Pipeline
+from src.supply_chain_optimization.pipelines.training_pipeline import training_Pipeline
 
 if __name__=="__main__":
     logging.info("The execution started")
@@ -12,7 +12,7 @@ if __name__=="__main__":
         #data_ingestion_config=DataIngestionConfig()
         #data_ingestion=DataIngestion()
         #data_ingestion.initiate_data_ingestion()
-            elt = transformer_Pipeline()
+            elt = training_Pipeline()
             elt.execute_pipelines()
 
     except Exception as e:
